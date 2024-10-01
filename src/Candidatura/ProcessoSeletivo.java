dac8c501e71d1acee41cb6bdd4576ea322115258
Ryan Max <ryanshift22@gmail.com>
2024-10-01 11:37:57 -0300

    Criação da logica

--- src/Candidatura/ProcessoSeletivo.java
+++ src/Candidatura/ProcessoSeletivo.java
@@ -1,12 +1,33 @@
 package Candidatura;
+import java.sql.SQLOutput;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-
-        String[] candidatos = {"BRUNO","JAMES","MARCOS","TULIO", "JULIA","AGUSTO","JAMESON","WELLS"};
+        selecaoCandidatos();
 
     }
+
+    static void selecaoCandidatos(){
+        String[] candidatos = {"BRUNO","MARIELE","MARIA","ENZO","JAMES","MARCOS","TULIO", "JULIA","AGUSTO","JAMESON","WELLS"};
+        double salarioBase=2200.0;
+        int candidatosSelecionados= 0;
+        int candidatosAtual= 0;
+        while (candidatosSelecionados< 5 && candidatosAtual <candidatos.length){
+            String candidato = candidatos[candidatosAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("o candidato " +candidato+ " solicitou este valor pretendido "+salarioPretendido);
+            if (salarioBase>= salarioPretendido){
+                System.out.println("o candidato " + candidato + " foi selecionado para a vaga ");
+                candidatosSelecionados++;
+            }
+            candidatosAtual++;
+        }
+
+    }
+
+
 
     static double valorPretendido(){
         return ThreadLocalRandom.current().nextDouble(1800, 2000);
